4e8620bea12b588b70fcb63c187f7aa3fa3a0810
kahofujita <kfujita00@mylangara.ca>
2022-06-17 21:26:23 -0700

    Firebase Config added

---
+++ Desktop/mygreen/js/firebase/firebase-config.js
@@ -0,0 +1,26 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDiw7ZDS3pMMG8mk3cPa7C_Tlq7ZqBgbz4",
+    authDomain: "mygreen-be569.firebaseapp.com",
+    projectId: "mygreen-be569",
+    storageBucket: "mygreen-be569.appspot.com",
+    messagingSenderId: "251959047157",
+    appId: "1:251959047157:web:b250edff8856a28a6ce6bc",
+    measurementId: "G-2X66VZR13E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+
+// Export to other files
+export { app, db, collection, addDoc }
