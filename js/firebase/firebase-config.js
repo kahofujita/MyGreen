6bc24f9dc8a5387939c4a8672296330fd1515d90
kahofujita <kfujita00@mylangara.ca>
2022-06-18 23:30:54 -0700

    Firebase config updated

--- js/firebase/firebase-config.js
+++ js/firebase/firebase-config.js
@@ -1,7 +1,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-analytics.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
