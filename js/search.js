219fccbb8d233023feca6791fd134edb27b91802
rojintag <106949675+rojintag@users.noreply.github.com>
2022-07-11 14:01:45 -0700

    change the import part of files
    make the same way as others for config firebase

--- js/search.js
+++ js/search.js
@@ -1,26 +1,7 @@
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
+import { db, auth } from './js/firebase/firebase-config.js';
 import { getFirestore, collection, query, where, getDocs, } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDiw7ZDS3pMMG8mk3cPa7C_Tlq7ZqBgbz4",
-  authDomain: "mygreen-be569.firebaseapp.com",
-  projectId: "mygreen-be569",
-  storageBucket: "mygreen-be569.appspot.com",
-  messagingSenderId: "251959047157",
-  appId: "1:251959047157:web:b250edff8856a28a6ce6bc",
-  measurementId: "G-2X66VZR13E"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-//init services
-const db = getFirestore(app);
 
 //collection Ref
 const colRef = collection(db, 'plant_ourinfo');
--- js/sugesstion.js
+++ js/sugesstion.js
@@ -2,31 +2,8 @@
 
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
-import { getFirestore, collection, getDocs, query, onSnapshot, where } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDiw7ZDS3pMMG8mk3cPa7C_Tlq7ZqBgbz4",
-  authDomain: "mygreen-be569.firebaseapp.com",
-  projectId: "mygreen-be569",
-  storageBucket: "mygreen-be569.appspot.com",
-  messagingSenderId: "251959047157",
-  appId: "1:251959047157:web:b250edff8856a28a6ce6bc",
-  measurementId: "G-2X66VZR13E"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-//init services
-const db = getFirestore(app);
-
+import { db, auth } from './js/firebase/firebase-config.js';
+import { collection, getDocs, query, onSnapshot, where } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js";
 
 //collection Ref
 const colRef = collection(db, 'plant_ourinfo');
