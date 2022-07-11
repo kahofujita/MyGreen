1370f5a3929fa31a5cf0ac2e94f112f1dd024298
Gabriel Dias <106842108+gabrieldiasls@users.noreply.github.com>
2022-07-11 12:07:17 -0700

    Update firebase-config.js

--- js/firebase/firebase-config.js
+++ js/firebase/firebase-config.js
@@ -2,6 +2,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,7 +22,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
+const auth = getAuth(app)
 
 
 // Export to other files
-export { app, db, collection, addDoc }
+export { app, db, collection, addDoc, auth }
