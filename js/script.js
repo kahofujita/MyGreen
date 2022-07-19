7ffcd912a0cddeed5a0c38e37155f00220f982ed
Kaho Fujita <105876236+kahofujita@users.noreply.github.com>
2022-07-19 10:04:24 -0700

    Updated SPA and styling

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@
 
 //setting up the Router with pages
 Router.init('mainArea', [
-  new Page('#home', 'homepage.html'), // 1st Page is default if no URL match
-  new Page('#myplants', 'myplants.html'),
+  new Page('#home', 'homepage.html', '', 'My Green - Home'), // 1st Page is default if no URL match
+  new Page('#myplants', 'myplants.html', '', 'My Green - My Plants'),
   // add new pages here
-  new Page('#public_journal', 'public_journal.html'),
-  new Page('#personal_journal', 'personal_journal.html'),
-  new Page('#sugesstion', 'sugesstion.html'),
-  new Page('#myaccount', 'myaccount.html')
+  new Page('#public_journal', 'public_journal.html', '', 'My Green - Journal'),
+  new Page('#personal_journal', 'personal_journal.html', '', 'My Green - My Journal'),
+  new Page('#sugesstion', 'sugesstion.html', '', 'My Green - Plant pick'),
+  new Page('#myaccount', 'myaccount.html', '', 'My Green - My Account'),
+  new Page('#search', 'search.html', '', 'My Green - Search')
 ]);
