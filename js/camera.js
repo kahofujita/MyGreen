8b5a6caa57576d71cc18f5ee5feefe341880c984
Kaho Fujita <105876236+kahofujita@users.noreply.github.com>
2022-07-20 09:24:29 -0700

    Adding Journal

--- js/camera.js
+++ js/camera.js
@@ -1,23 +1,17 @@
 import {db, auth} from './firebase/firebase-config.js'
-
 import {collection, addDoc, doc, getDoc, query, where, getDocs} from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js";
 class Note {
     #date;
-
-    constructor( date, caption, journal, imageURL) {
-
-        this.date = date;
-        this.caption = caption;
-        this.journal = journal;
+    constructor( imageURL,caption,date , journal, ) {
         this.imageURL = imageURL;
+        this.caption = caption;
+        this.date = date;
+        this.journal = journal;
     }
-
     get date() {
       return this.#date;
-    } 
-
+    }
     set date( date) {
         const now = new Date();
         console.log(now);
@@ -28,38 +22,29 @@ class Note {
             // alart("You can not input feature date");
         }
     }
-
     toString() {
-        return `Date: ${this.date}, 
-                Caption: ${this.caption}, 
-                journal: ${this.journal},  
-                Image URL: ${this.imageURL}`;
+        return ` Image URL: ${this.imageURL},
+                Caption: ${this.caption},
+                Date: ${this.date},
+                journal: ${this.journal},`;
     }
-
 }
-
 ////Get Personal ID /////
 let userId = sessionStorage.getItem('userID');
 console.log(sessionStorage.getItem('userID'));
-
 //GET USER NAME
 const userName = document.getElementById('username');
-
-// 
-
+//
 let usernameArray = [];
 const un = await getDocs(collection(db, "user_info"));
-
 un.forEach((doc) => {
  usernameArray.push(doc.data());
 });
-
-console.log(usernameArray); 
+console.log(usernameArray);
 let j = 0;
 const username = usernameArray[j].avatar_img_name;
 for(let j = 0; usernameArray.length > j; j++){
-
-  if (usernameArray[j].user_id == userId){ 
+  if (usernameArray[j].user_id == userId){
         userName.innerText = usernameArray[j].username;
   }else{
   };
@@ -72,81 +57,62 @@ for(let j = 0; usernameArray.length > j; j++){
 // const jId = await getDocs (jCollection);
 // const jIds = await getDocs(jId);
 // const db = firebase.firestore();
-
 const docRef = doc(db, "journal", "Um2tjbRjsK917w1tjSYJ");
-
 const docSnap = await getDoc(docRef);
-console.log(docSnap.data().caption);
-
-
+// console.log(docSnap.data().caption);
 // db.collection("journal").get().then((querySnapshot) => {
 //   querySnapshot.forEach((doc) => {
 //       console.log(doc.id, " => ", doc.data());
 //   });
 // });
-
-
-
 ////SUBMIT //////
 const form1 = document.getElementById("form1");
-
 form1.addEventListener("submit", function (event) {
     event.preventDefault();
     const dateInput = form1.querySelector("#date");
     const captionInput = form1.querySelector("#caption");
     const journalInput = form1.querySelector("#journal");
     const imageInput = form1.querySelector("#image");
-
-
-    const note = new Note( dateInput.value, captionInput.value,
-                            journalInput.value, imageInput.value );
-    console.info(note.toString());   
-
+    const note = new Note( imageInput.value, captionInput.value, dateInput.value,
+                            journalInput.value  );
+    console.info(note.toString());
     const back = document.getElementById("back");
     back.innerHTML = renderDogAsHTML(note);
-
-    
+    const card = document.getElementById("card");
+    card.style.display = "none";
     //ADD DATA into Firebase
-    
     const colUserInfo = collection(db, 'journal');
     addDoc(colUserInfo, {
-      journal_date:form1.date.value,  
-            caption:form1.caption.value,
-            care_instruction:form1.journal.value,
             picture_img_name: form1.image.value,
+            caption:form1.caption.value,
+            journal_date:form1.date.value,
+            care_instruction:form1.journal.value,
             // journal_id:jId,
-
             user_id:userId
-            
     })
-    
-
 })
-
 function renderDogAsHTML( note ) {
-    if ((!note instanceof Note)) { 
+    if ((!note instanceof Note)) {
         return "<div>Not a Valid Dog Object</div>";
     }
-    let htmlBlock = `<div>
-            <p>Date: ${note.date} </p>        
-            <p>Caption: ${note.caption} </p>
-            <p>journal: ${note.journal} </p>
-            </div>
-            <hr>`;
+    let htmlBlock = `<div class="results">
+            <p class="titles">Caption</p>
+            <p>${note.caption} </p>
+            <p class="dates">${note.date} </p>
+            <p class="titles">Care Instructions</p>
+            <p>${note.journal} </p>
+            </div>`;
             console.log(htmlBlock);
-  
     if (note.imageURL){
-        htmlBlock += `<div><img src="${note.imageURL}" width="400" ></div>`;
+        htmlBlock += `<div id="final-img"><img src="${note.imageURL}" width="400" ></div>`;
     } else {
         htmlBlock += `<div>No Image Provided</div>`;
     }
     return htmlBlock;
 }
-
 ///Change page  ////////
 const allPages = document.querySelectorAll("div.page");
 navigateToPage();
-
 function navigateToPage(event) {
     const pageId = location.hash? location.hash : '#profile';
     for(let page of allPages) {
@@ -158,47 +124,29 @@ function navigateToPage(event) {
     }
     return;
 }
-
 ///camera system camera on & snap    /////////
-
 window.addEventListener("hashchange", navigateToPage);
-
-
 const video = document.getElementById('video');
-
-
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 context.scale(0.5, 0.5);
-
-
-
 document.getElementById("start").addEventListener("click", function () {
-  
   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  
     navigator.mediaDevices.getUserMedia({ video: true }).then( (stream) => {
-      
       video.srcObject = stream;
-     
     });
   } else {
     console.log("media devices not available in this browser");
   }
   videoFlame.style.display = "block";
 });
-
 ////Change camera front & back
-
   // const videos = document.querySelector("#camera");
   // let useFront = true;     // フロントカメラ:true, バックカメラ:false
-
-
   // document.querySelector("#btn-toggle").addEventListener("click", ()=>{
   //   syncCamera(videos, useFront);
   //   useFront = !useFront;      // boolean値を反転
   // });
-
   // function syncCamera(videos, is_front=true){
   //   // 前後カメラの設定
   //   let facingMode = null;
@@ -206,74 +154,35 @@ document.getElementById("start").addEventListener("click", function () {
   // }
 const videoFlame = document.getElementById("video");
 const cameraOnBtn = document.getElementById("start");
-
 //////SNAP PICTURE////////
 document.getElementById("snap").addEventListener("click",  () => {
-  
   context.drawImage(video, 0, 0,);
   const imageBlob = canvas.toBlob(handleBlob, 'image/jpeg');
-  
   //stop camera automatically
   const tracks = video.srcObject.getTracks();
   tracks.forEach(track => track.stop());
-
   videoFlame.style.display = "none";
-  cameraOnBtn.innerText = "Take a picture again";
-
+  cameraOnBtn.innerText = "Snap Again";
 });
-
 // document.getElementById("stop").addEventListener("click",  ()=> {
 //   const tracks = video.srcObject.getTracks();
 //   tracks.forEach(track => track.stop());
 // });
-
-
-
 // Get a reference to the storage service, which is used to create references in your storage bucket
 // const storage = getStorage();
-
 // // // Create a storage reference from our storage service
 // const storageRef = ref(storage, 'journal');
-
 //     // 'file' comes from the Blob or File API
 // uploadBytes(storageRef, file).then((snapshot) => {
 //   console.log('Uploaded a blob or file!');
 // });
-
 // ///////
-
-
-
 function handleBlob(blob) {
-   
     const objectURL = window.URL.createObjectURL(blob);
-
-
     const reader = new FileReader();
     reader.addEventListener('load', () => {
       console.log(reader.result);
-    
       document.getElementById("image").value = reader.result;
     });
-    reader.readAsDataURL(blob); 
-   
+    reader.readAsDataURL(blob);
   }
-
-
-//   document.getElementById("menu_toggle").addEventListener("click", menuToggler);
-
-//   function menuToggler() {
-//     document.getElementById("menu").classList.toggle("show_menu");
-// }
-
-// (async () => {
-//   try {
-
-//     const userRef = await db.collection('journal').doc('wjJ0hWsTUcl8gspsqRcM')
-//     await userRef.set({
-//       picture_img_name:'image'.value,
-
-//     })
-
-//   }
-// }
--- js/personal_journal.js
+++ js/personal_journal.js
@@ -1,70 +1,48 @@
 import { db, auth} from './firebase/firebase-config.js'
-
 import {collection, addDoc, doc, getDoc, query, where, getDocs} from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js";
-
-
 const journalArray = [];
 const personalArray = [];
-
+let  yourId;
+// --------
+// const active = document.activeElement;
+// le yourId;
 ///特定の人のjournal InfoのみをP/Uしてくる
-
 ////Get Personal ID FROM LOGIN/////
 let yourUserId = sessionStorage.getItem('userID');
 console.log(yourUserId);
-
-
 ///GET JOURNALS FROM DB /////
 const journal = await getDocs(collection(db, "journal"));
 let journalPost;
 journal.forEach((doc) => {
 //  let myId = doc.data().user_id;
-
 //journal全部が入ったarray
- journalArray.push(doc.data()); 
-
+ journalArray.push(doc.data());
 });
-
 ///IDENTIFY WHO'S JOURNAL IS THIS　
 for(let i = 0; journalArray.length > i; i++){
-  
   if (journalArray[i].user_id == yourUserId){
-         
           personalArray.push(journalArray[i]);
       }else{
-        
 }
-
 }
-
 console.log(personalArray);
-
 //GET USER NAME
 const userName = document.getElementById('username');
-
-
 let usernameArray = [];
 const un = await getDocs(collection(db, "user_info"));
-
 un.forEach((doc) => {
-
-
  usernameArray.push(doc.data());
 });
-
-console.log(usernameArray); 
+console.log(usernameArray);
 let j = 0;
 const username = usernameArray[j].avatar_img_name;
 for(let j = 0; usernameArray.length > j; j++){
-
-  if (usernameArray[j].user_id == yourUserId){ 
+  if (usernameArray[j].user_id == yourUserId){
         userName.innerText = usernameArray[j].username;
   }else{
   };
 }
-
-
 //POST NUMBER //
 const postNbr = document.getElementById("postNbr");
 if (personalArray.length <= 1){
@@ -72,132 +50,104 @@ if (personalArray.length <= 1){
 }else{
   postNbr.innerHTML = `${personalArray.length}Posts`;
 }
-
-
 let i = 0;
 function Nbrs(){
   const Nbr = personalArray.indexOf(personalArray[i]);
 }
-
 //GET JOURNAL IDs
 const jId = [];
 const querySnapshot = await getDocs(collection(db, 'journal'));
 querySnapshot.forEach((doc) => {
-  jId.push(doc.id);  
+  jId.push(doc.id);
 })
 console.log(jId);
-
-
-
+let instruction;
 //DISPLAY ALL JOURNAL ////
 for(let i = 0; personalArray.length > i; i++ ){
-   
   const Nbr = personalArray.indexOf(personalArray[i]);
-
-
-      const infoArea = document.getElementById("post");          
+      const infoArea = document.getElementById("post");
       const article = document.createElement("div");
       const divTag = article.classList.add("journal");
       const divClass = article.setAttribute('id', i );
       infoArea.appendChild(article);
-     
-       
       const click_tag = document.createElement("a");
-   
-    
     click_tag.href = "#single_page";
     console.log(click_tag);
     const aaa = click_tag.setAttribute('class', 'single');
-   
     const ok = click_tag.setAttribute('id', jId[i]);
       article.appendChild(click_tag);
-    
       const picture = document.createElement("img");
+      const pictures = click_tag.setAttribute('class', 'single');
       picture.src = personalArray[i].picture_img_name;
       picture.innerText;
     click_tag.appendChild(picture);
-      
-
-
-      const cap = document.createElement("caption");
+      const cap = document.createElement("p");
       cap.innerText = personalArray[i].caption;
       article.appendChild(cap);
-
-
       const date = document.createElement("p");
       date.innerText = personalArray[i].journal_date;
       article.appendChild(date);
-  
-      const instruction = document.createElement("p");
+      instruction = document.createElement("p");
+      instruction.setAttribute('class', 'inst');
       instruction.innerText = personalArray[i].care_instruction;
       article.appendChild(instruction);
-    
-    
+    console.log(instruction);
+    // instruction.style.display = "none";
     }
-    
-    
-    
-   
     const Nbr = personalArray.indexOf(personalArray[i]);
     console.log(Nbr);
-  
    const hidePage = document.getElementById("result");
-
-  
   // RESULT PAGE HIDE WHEN WHOLE PAGE OPEN//////
   const resultPage = document.getElementById('result');
   resultPage.style.display = "none";
-
   const comment = document.getElementById('comment');
-  comment.style.display = "none";  
-
+  comment.style.display = "none";
   const backBtn = document.getElementById('btn');
   backBtn.style.display = "none";
-
-
+  const add_post_btn = document.getElementById('go-camera');
     ///MOVE TO SINGLE PAGE  EventListener /////
   console.log(result);
-  const atag = document.getElementsByClassName('single'); 
+  const atag = document.getElementsByClassName('single');
   console.log(atag);
   [...atag].forEach(single_page => {
     single_page.addEventListener('click', (e)=>{
       result.innerText = ' hello';
-     
       console.log(single_page.parentElement);
       const nikki = single_page.parentElement;
       const nikkis = nikki.innerHTML;
- 
       result.innerHTML = nikkis;
-
       //The ID You Clicked NOW! = Journal ID
       const active = document.activeElement;
-      const yourId = active.id; 
+      yourId = active.id;
       console.log(yourId);
-   
-   
     //////CHANGE PAGE //////
-    
     navigateToPage();
-    
+    displayCmt();
+    for (let i = 0; cmtArray.length > i; i++){
+      if(yourId == cmtArray[i].journal_id){
+        displayCmt();
+      }else{
+      }
+    }
     })
   })
-
+  console.log(yourId);
   window.addEventListener("hashchange", navigateToPage);
-  
   const pageId = document.getElementById("post");
   const allPages = document.querySelectorAll("div.page");
-  
+  // const single_img = document.querySelectorAll('img');
+  const HnS = document.getElementsByClassName("inst");
+     console.log(HnS);
   console.log(allPages);
-  
   function navigateToPage(event) {
-   
     pageId.style.display = "none";
     resultPage.style.display = "block";
     backBtn.style.display = "block";
     postNbr.style.display = "none";
     comment.style.display = "block";
+    add_post_btn.style.display = "none";
+    // instruction.style.display = "block";
     console.log(pageId);
-    
 }
 backBtn.addEventListener('click', (event)=>{
   resultPage.style.display = "none";
@@ -205,68 +155,38 @@ backBtn.addEventListener('click', (event)=>{
   pageId.style.display = "block";
   postNbr.style.display = "block";
   comment.style.display = "none";
+  add_post_btn.style.display = "block";
+  instruction.style.display = "none";
 })
-
-//COMMENT ======================================// 
+//COMMENT ======================================//
 const comments = document.getElementById('comment');
-
 //GET COMMENT DB
 const cmtArray = [];
-
 const cmt = await getDocs(collection(db, "comment"));
 cmt.forEach((doc) => {
-
-  
-
   cmtArray.push(doc.data());
 });
 console.log(cmtArray);
-
-
 //Display Comments & Username
 function displayCmt(){
-  const cmt_display = document.getElementById("cmt_display");  
-  
-    const who_write =  document.createElement("p");
-    who_write.innerText = cmtArray[i].username;
-    cmt_display.appendChild(who_write);
-
-    const yourcmt =  document.createElement("p");
-    yourcmt.innerText = cmtArray[i].comment;
-    cmt_display.appendChild(yourcmt);
+  const cmt_display = document.getElementById("cmt_display");
+    cmt_display.innerHTML += `<div class="cmts"><p>${cmtArray[i].username}</p><p>${cmtArray[i].comment}<P></div>`;
 };
-
-  
+  //ここで今開いてるJournalのIDとコメントアレーの中のJr IFが同じやつだけpickupして表示させる＊database に入れる！
   //COMMMET Journal_ id & this clicked Journal ID is same DISPLAY
-  
-  for (let i = 0; cmtArray.length > i; i++){
-
-    if(yourId == cmtArray[i].journal_id){
-      displayCmt();
-    }else{
-
-    }
-  }
-
   ////Get USERNAME FROM LOGIN ID///// commentの記入者を特定
   let userId = sessionStorage.getItem('userID');
   console.log(sessionStorage.getItem('userID'));
-
   const uns = doc(db, "user_info", userId);
   const uninfo =  await getDoc(uns);
   let yourname = uninfo.data().username;
   console.log(yourname);
-
   //SUBMIT COMMENT ======================
-
-
 // getCmtBtn();
 const cmtarea =document.getElementById('write_cmt');
 const submitBtn = document.getElementById('submiting');
 console.log(submitBtn);
 submitBtn.addEventListener('click', (e) =>{
-
-
   //ADD COMMENT INTO DB
   const commentdb = collection(db, 'comment');
   addDoc(commentdb, {
@@ -275,9 +195,9 @@ submitBtn.addEventListener('click', (e) =>{
       username:yourname,
       journal_id:yourId
   })
-
-  //ADD NEW COMMENT INTO COMMENT FIELD
-  displayCmt();
+  cmt_display.innerHTML += `<div class="cmts"><p>${yourname} :</p><p>${cmtarea.value}<P></div>`;
   cmtarea.innerHTML = "";
-
   })
+
+
+
