a73efd75c4df6874d5a422a249164528cd71f6ce
Gabriel Dias <gaabrieldls@gmail.com>
2022-07-20 09:23:02 -0700

    styling and addaplant

--- js/addplant.js
+++ js/addplant.js
@@ -3,37 +3,24 @@ import {
 } from "./firebase/firebase-config.js";
 
 import {
-    auth
-} from "./firebase/firebase-config.js";
-
-import {
     collection,
     getDocs,
     doc,
+    onSnapshot,
     setDoc,
     query,
     where
 } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js";
 
-import {
-    createUserWithEmailAndPassword,
-    onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js";
+sessionStorage.setItem('userID', 'ZbvOuGroV8bEMXeLr55apNJFCCq1')
+const userId = sessionStorage.getItem('userID');
 
-// If the user already logged in move him to homepage
-
-if (sessionStorage.getItem('userID') !== null) {
-    window.location.assign("./homepage.html");
-}
-
-// collection ref
-const colUserInfo = collection(db, 'user_info')
 const colPlantUserInfo = collection(db, 'plant_userinfo')
 const colPlantOurInfo = collection(db, 'plant_ourinfo')
 
-// Populating the Plant Names inside the form
 const plantOurInfo = [];
 const plantNameArr = [];
+let plantUserInfo = [];
 
 await getDocs(colPlantOurInfo)
     .then((snapshot) => {
@@ -65,6 +52,38 @@ await getDocs(colPlantOurInfo)
         console.log(err.message)
     })
 
+    onSnapshot(doc(db, "plant_userinfo", userId), (doc) => {
+        let plantsUpdate = (doc.data().plantsList)
+        
+        for (let plant of plantsUpdate) {
+            plantUserInfo.push(plant)
+        }
+
+        console.log(plantUserInfo)
+    })
+        // doc.docs.forEach((doc) => {
+        //     if (doc.id === userId) {
+        //     console.log(doc.id)
+        //     plantUserInfo.push(
+        //         doc.data().plantsList[0]
+        //     )}
+        // })})
+
+    // await getDocs(colPlantUserInfo)
+    // .then((snapshot) => {
+    //     snapshot.docs.forEach((doc) => {
+    //         if (doc.id === userId) {
+    //         console.log(doc.id)
+    //         plantUserInfo.push(
+    //             doc.data().plantsList[0]
+    //         )}
+    //     })
+    //     console.log(plantUserInfo)
+    // })
+    // .catch(err => {
+    //     console.log(err.message)
+    // })
+
 const outdoorPlantsArr = [];
 const indoorPlantsArr = [];
 
@@ -88,82 +107,10 @@ querySnapshotIndoor.forEach((doc) => {
     indoorPlantsArr.push(doc.data());
 });
 
-// Signup the user
-
-const signupForm = document.querySelector('.signup')
-
-signupForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    // Variables of the signup form
-
-    // Page 1
-    const email = document.getElementById('emailSignup').value
-    const password = document.getElementById('passwordSignup').value
-    const confirmPassword = document.getElementById('confirmPasswordSignup').value
-
-    // Page 2
-    const username = document.getElementById('usernameSignup').value
-    const fname = document.getElementById('fnameSignup').value
-    const lname = document.getElementById('lnameSignup').value
-
-    // Page 3
-    let avatar = "";
-    const radiosAvatar = document.querySelectorAll('.avatarRadio');
-    for (let radio of radiosAvatar) {
-        if (radio.checked) {
-            avatar = radio.value;
-        }
-    }
-
-    // Creating user on authentication and user_info on Firebase
-
-    if (email === '' || password === '' || confirmPassword === '' || username === '' || fname === '' || lname === '') {
-        alert("Please fill the whole form");
-    } else if (password.length < 6) {
-        alert("Password must be at least 6 characters");
-    } else if (password === confirmPassword) {
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((cred) => {
-                setDoc(doc(db, "user_info", cred.user.uid), {
-                    email: email,
-                    username: username,
-                    fname: fname,
-                    lname: lname,
-                    avatar_img_name: avatar,
-                    user_id: cred.user.uid
-                })
-                sessionStorage.setItem('userID', cred.user.uid);
-
-            }).then(() => {
-
-                signupForm.reset()
-
-                const signupForm3 = document.querySelector('.signup-page3-wrapper');
-                const addPlantForm = document.querySelector('.addpPLantForm');
-
-                signupForm3.classList.remove('show')
-                signupForm3.classList.toggle('left')
-                addPlantForm.classList.remove('right')
-                addPlantForm.classList.toggle('show')
-
-                alert("User Created");
-            })
-            .catch((err) => {
-                console.log(err.message)
-                alert('Email already in use')
-            })
-    } else {
-        alert("Passwords Don't Match");
-    }
-})
-
-// ADDING A NEW PLANT
-
 const addPlantBtn = document.getElementById('addPlantButton');
-const addPlantForm = document.querySelector('.addpPLantForm');
+const addPlantForm = document.querySelector('.addpPlantForm');
 
-const plantsList = [];
+
 
 addPlantBtn.addEventListener('click', () => {
 
@@ -208,10 +155,10 @@ addPlantBtn.addEventListener('click', () => {
         plant_id: plant_id
     }
 
-    plantsList.push(plant);
+    plantUserInfo.push(plant);
 
     // print every time you add to the array
-    console.log(plantsList)
+    console.log(plantUserInfo)
 
     addPlantForm.reset()
 })
@@ -219,12 +166,11 @@ addPlantBtn.addEventListener('click', () => {
 addPlantForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const userId = sessionStorage.getItem('userID');
-
     setDoc(doc(db, "plant_userinfo", userId), {
-        plantsList: plantsList
+        plantsList: plantUserInfo
     })
 
-    alert("Signup Finished");
-    window.location.assign("./index.html");
+    alert("Plants added");
+    addPlantForm.reset();
+    plantUserInfo = [];
 })
