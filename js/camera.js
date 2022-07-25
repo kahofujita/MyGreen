c3f59d3c346b03c8cfbdf18e80ecacdc61da8473
Gabriel Dias <gaabrieldls@gmail.com>
2022-07-25 15:55:25 -0700

    adding all info

---
+++ js/camera.js
@@ -0,0 +1,227 @@
+import {db, auth} from './firebase/firebase-config.js'
+
+import {collection, addDoc, doc, getDoc, query, where, getDocs} from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js";
+class Note {
+    #date;
+
+    constructor( imageURL,caption,date , journal, ) {
+        
+        this.imageURL = imageURL;
+        this.caption = caption;
+        this.date = date;
+        this.journal = journal;
+        
+    }
+
+    get date() {
+      return this.#date;
+    } 
+
+    set date( date) {
+        const now = new Date();
+        console.log(now);
+        if (new Date(date) <= now) {
+            this.#date = date;
+        } else {
+            console.error("invalid Date is provided: "+date);
+            // alart("You can not input feature date");
+        }
+    }
+
+    toString() {
+        return ` Image URL: ${this.imageURL},
+                Caption: ${this.caption},
+                Date: ${this.date},  
+                journal: ${this.journal},`;
+    }
+
+}
+
+////Get Personal ID /////
+let userId = sessionStorage.getItem('userID');
+console.log(sessionStorage.getItem('userID'));
+
+//GET USER NAME
+const userName = document.getElementById('username');
+
+
+
+let usernameArray = [];
+const un = await getDocs(collection(db, "user_info"));
+
+un.forEach((doc) => {
+ usernameArray.push(doc.data());
+});
+
+console.log(usernameArray); 
+let j = 0;
+const username = usernameArray[j].avatar_img_name;
+for(let j = 0; usernameArray.length > j; j++){
+
+  if (usernameArray[j].user_id == userId){ 
+        userName.innerText = usernameArray[j].username;
+  }else{
+  };
+}
+
+
+const docRef = doc(db, "journal", "Um2tjbRjsK917w1tjSYJ");
+
+const docSnap = await getDoc(docRef);
+
+
+////SUBMIT //////
+const form1 = document.getElementById("form1");
+
+form1.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const dateInput = form1.querySelector("#date");
+    const captionInput = form1.querySelector("#caption");
+    const journalInput = form1.querySelector("#journal");
+    const imageInput = form1.querySelector("#image");
+
+
+    const note = new Note( imageInput.value, captionInput.value, dateInput.value,
+                            journalInput.value  );
+    console.info(note.toString());   
+
+    const back = document.getElementById("back");
+    back.innerHTML = renderDogAsHTML(note);
+
+    const card = document.getElementById("card");
+    card.style.display = "none";
+
+      
+    //ADD DATA into Firebase
+    
+    const colUserInfo = collection(db, 'journal');
+    addDoc(colUserInfo, {
+            picture_img_name: form1.image.value,
+            caption:form1.caption.value,
+            journal_date:form1.date.value,  
+            care_instruction:form1.journal.value,
+            user_id:userId      
+    })
+})
+
+function renderDogAsHTML( note ) {
+    if ((!note instanceof Note)) { 
+        return "<div>Not a Valid Dog Object</div>";
+    }
+    let htmlBlock = `<div class="results">
+            <p class="titles">Caption</p>
+            <p>${note.caption} </p>
+            <p class="dates">${note.date} </p>        
+            <p class="titles">Care Instructions</p>
+            <p>${note.journal} </p>
+            </div>`;
+            console.log(htmlBlock);
+  
+    if (note.imageURL){
+        htmlBlock += `<div id="final-img"><img src="${note.imageURL}" width="400" ></div>`;
+    } else {
+        htmlBlock += `<div>No Image Provided</div>`;
+    }
+    return htmlBlock;
+   
+}
+
+///Change page  ////////
+const allPages = document.querySelectorAll("div.page");
+navigateToPage();
+
+function navigateToPage(event) {
+    const pageId = location.hash? location.hash : '#profile';
+    for(let page of allPages) {
+        if (pageId === '#'+page.id) {
+            page.style.display = "block";
+        } else {
+            page.style.display = "none";
+        }
+    }
+    return;
+}
+
+///camera system camera on & snap    /////////
+
+window.addEventListener("hashchange", navigateToPage);
+
+
+const video = document.getElementById('video');
+
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+context.scale(0.5, 0.5);
+// context.scale(1, 1);
+
+
+
+document.getElementById("start").addEventListener("click", function () {
+  
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  
+    navigator.mediaDevices.getUserMedia({ video: true }).then( (stream) => {
+      
+      video.srcObject = stream;
+     
+    });
+  } else {
+    console.log("media devices not available in this browser");
+  }
+  videoFlame.style.display = "block";
+});
+
+////Change camera front & back
+
+  // const videos = document.querySelector("#camera");
+  // let useFront = true;     // フロントカメラ:true, バックカメラ:false
+
+
+  // document.querySelector("#btn-toggle").addEventListener("click", ()=>{
+  //   syncCamera(videos, useFront);
+  //   useFront = !useFront;      // boolean値を反転
+  // });
+
+  // function syncCamera(videos, is_front=true){
+  //   // 前後カメラの設定
+  //   let facingMode = null;
+  // facingMode = (is_front)?  "user":{ exact: "environment" };
+  // }
+const videoFlame = document.getElementById("video");
+const cameraOnBtn = document.getElementById("start");
+
+//////SNAP PICTURE////////
+document.getElementById("snap").addEventListener("click",  () => {
+  
+  context.drawImage(video, 0, 0,);
+  const imageBlob = canvas.toBlob(handleBlob, 'image/jpeg');
+  
+  //stop camera automatically
+  const tracks = video.srcObject.getTracks();
+  tracks.forEach(track => track.stop());
+
+  videoFlame.style.display = "none";
+  cameraOnBtn.innerText = "Snap Again";
+
+});
+
+
+function handleBlob(blob) {
+   
+    const objectURL = window.URL.createObjectURL(blob);
+
+
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      console.log(reader.result);
+    
+      document.getElementById("image").value = reader.result;
+    });
+    reader.readAsDataURL(blob); 
+   
+  }
+
+
---
+++ js/search.js
@@ -0,0 +1,70 @@
+
+// Import the functions you need from the SDKs you need
+import { db, auth, onAuthStateChanged, collection, query, where, getDocs } from './firebase/firebase-config.js';
+
+//collection Ref
+const colRef = collection(db, 'plant_ourinfo');
+
+//get collection data
+let plantOurinfo = [];
+await getDocs(colRef)
+  .then((snapshot) => {
+    console.log(snapshot.docs);
+
+    snapshot.docs.forEach((doc) => {
+      plantOurinfo.push({ ...doc.data(), id: doc.id });
+    })
+    console.log(plantOurinfo);
+  })
+  .catch(err => {
+    console.log(err.message);
+  })
+
+
+let reg = (input, text) => {
+  let flags = 'gi';
+  let regString = new RegExp(input, flags)
+  // console.log(regString)
+  const result = regString.test(text)
+  // console.log(result)
+  return result
+}
+
+let timer
+const inputSearch = document.querySelector('#search-input')
+inputSearch.addEventListener('input', (e) => {//try change it will search all of them , click button, keyup when you hit enter on the keyboard
+  if (!timer) {
+    timer = setTimeout(() => {
+      plantOurinfo.map(plant => {
+        if (reg(e.target.value, plant.plant_name)) {
+          let link = window.location.protocol + "//" + window.location.host + `/plant-detail.html?name=${plant.plant_name.replace(/ /g, "%20")}`;
+          document.querySelector('.result').innerHTML += `<li>${plant.plant_name}(${plant.location}).<a href="${link}">the details</a></li>`;
+        }
+      })
+
+    }, 500)
+  } else {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      plantOurinfo.map(plant => {
+        if (reg(e.target.value, plant.plant_name)) {
+          let link = window.location.protocol + "//" + window.location.host + `/plant-detail.html?name=${plant.plant_name.replace(/ /g, "%20")}`;
+          document.querySelector('.result').innerHTML += `<li>${plant.plant_name}(${plant.location}).<a href="${link}">the details</a></li>`;
+         
+       
+console.log(link);
+
+    
+        
+        }
+      })
+      
+    }, 500)
+
+    
+  }
+});
+
+inputSearch.value = "";
+
+
